1685583cd14a52aae3180245a2a273f1437149c7
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-02-04 16:20:10 -0300

    feat: add '50 Minutes' label with custom font to the application

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from tkinter import *
 
 class Application:
     def __init__(self, master=None):
-        pass
+        self.widget1 = Frame(master)
+        self.widget1.pack()
+        self.msg = Label(self.widget1, text='50 Minutes')
+        self.msg["font"] = ("Times New Roman", "50")
+        self.msg.pack()
 
 root = Tk()
 Application(root)
