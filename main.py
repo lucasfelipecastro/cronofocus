a6e02e835f4f4e8bc3691813aa399e82198b831c
lucasfelipecastro <lucassantanafelipe@gmail.com>
2025-02-04 16:47:23 -0300

    feat(button): add 'Start Timer' button with custom font to the application

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ class Application:
         self.widget1 = Frame(master)
         self.widget1.pack()
         self.msg = Label(self.widget1, text='50 Minutes')
-        self.msg["font"] = ("Times New Roman", "50")
+        self.msg['font'] = ('Times New Roman', '50')
         self.msg.pack()
+
+        self.start = Button(self.widget1)
+        self.start['text'] = 'Start Timer'
+        self.start['font'] = ('Verdana', '30')
+        self.start['width'] = 10
+        self.start.pack()
 
 root = Tk()
 Application(root)
